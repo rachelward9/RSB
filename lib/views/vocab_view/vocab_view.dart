c5ad29538745c0079fba9b9303cd42c2ac68fbad
Unknown <mr.pinstripe_Suit@hotmail.com>
2017-08-01 18:55:55 -0600

    Boiners

---
+++ lib/views/vocab_view/vocab_view.dart
@@ -0,0 +1,31 @@
+
+//import 'dart:async';
+import 'package:angular2/angular2.dart';
+import 'package:angular_components/angular_components.dart';
+import '../vocab_list_component/vocab_list_service.dart';
+import 'package:angular_components/angular_components.dart';
+import '../vocab_list_component/vocab_list_component.dart';
+//import 'package:angular_components/src/components/material_tab/material_tab_panel.dart';
+//import 'package:angular_components/src/components/material_tab/material_tab.dart';
+//import 'vocab_list_service.dart';
+
+@Component(
+  selector: 'vocab-view',
+  styleUrls: const ['vocab_view.css'],
+  templateUrl: 'vocab_view.html',
+  directives: const [
+    CORE_DIRECTIVES,
+//    VocabListService,
+    VocabListComponent,
+    materialDirectives//,
+//    TodoListComponent
+  ],
+  providers: const [
+    materialProviders//,
+//    VocabListService
+  ],
+)
+
+class VocabView {
+
+}
